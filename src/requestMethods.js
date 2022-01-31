3dc9fab9e5c43d895892f20034c8871bde9a5cd2
Chinecherem Harry <chinecherempharry@gmail.com>
2022-01-31 01:48:34 +0100

    get user token from localstorage

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = " http://localhost:4000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWRmYjhkNGMwNzc3MjcwNGExOThhOCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MzU3Nzg3MiwiZXhwIjoxNjQzODM3MDcyfQ.of4xGxLh-Z2f5pVJdxyW6F3ePy-GJs3gQPsnFD0xocM";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+  .currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
